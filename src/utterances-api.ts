a57bdbfe03f7e83a1a0af7736736c6d39b93563d
Kyujin Cho <kyujin.cho@lablup.com>
2019-12-26 01:11:15 +0900

    Change API location

--- src/utterances-api.ts
+++ src/utterances-api.ts
@@ -1,2 +1,2 @@
 // export const UTTERANCES_API = 'http://localhost:5000';
-export const UTTERANCES_API = "https://utteranc.es";
+export const UTTERANCES_API = "https://utterances-api.bluehouse.dev";
